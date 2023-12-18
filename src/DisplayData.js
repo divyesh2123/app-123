31f374dd1b214793d5e197a1421f338b9a187109
lenovo <lenovo@LAPTOP-BLNTEBH7>
2023-12-18 09:56:22 +0530

    changes for the data

--- src/DisplayData.js
+++ src/DisplayData.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { DataGrid } from '@mui/x-data-grid';
+import { Button, Stack } from '@mui/material';
 
 export default function DisplayData(props) {
 
@@ -7,7 +8,39 @@ export default function DisplayData(props) {
         { field: 'firstName', headerName: 'first Name', width: 150 },
         { field: 'lastName', headerName: 'last Name', width: 150 },
         { field: 'Address1', headerName: 'Address1', width: 150 },
-        { field: 'Address2', headerName: 'Address2', width: 150 },
+        { field: 'Address2', headerName: 'Address2', width: 150
+    
+    
+    
+    },
+    {
+        
+            field: 'action',
+            headerName: 'Action',
+            width: 180,
+            sortable: false,
+            disableClickEventBubbling: true,
+            
+            renderCell: (params) => {
+                const onClick = (e) => {
+                  const currentRow = params.row;
+                  return alert(JSON.stringify(currentRow, null, 4));
+                };
+                
+                return (
+                  <Stack direction="row" spacing={2}>
+                    <Button variant="outlined" color="warning" size="small">Edit</Button>
+                    <Button variant="outlined" color="error" size="small" >Delete</Button>
+                  </Stack>
+                );
+            }
+
+    }
+
+    
+
+
+
       ];
 
   return (
